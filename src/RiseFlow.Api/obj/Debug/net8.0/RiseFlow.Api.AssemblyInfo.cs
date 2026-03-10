d47e0639c080a9f5fbdbd41ef63f324eccc73653
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-03-10 21:34:38 +0100

    update

--- src/RiseFlow.Api/obj/Debug/net8.0/RiseFlow.Api.AssemblyInfo.cs
+++ src/RiseFlow.Api/obj/Debug/net8.0/RiseFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiseFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c767dbb8c1de969574ff075b0fb39e31b26bcb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884d292a902614406cec9d2f53259f2fef106178")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiseFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiseFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
